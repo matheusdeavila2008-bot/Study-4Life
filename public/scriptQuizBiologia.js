3257221f35fe9634f26ef22409e7c0a185da1561
Matheus D'Ávila <matheusdeavila2008@gmail.com>
2026-05-29 13:53:35 -0300

    atlzç quiz perguntas

---
+++ public/scriptQuizBiologia.js
@@ -0,0 +1,51 @@
+// menu lateral
+const menu = document.getElementById("menuLateral");
+const menuIcon = document.querySelector(".menu-icon");
+
+function openMenu() {
+  menu.classList.add("active");
+}
+
+function closeMenu() {
+  menu.classList.remove("active");
+}
+
+document.addEventListener("click", (e) => {
+  if (!menu.contains(e.target) && !menuIcon.contains(e.target)) {
+    menu.classList.remove("active");
+  }
+});
+
+let faseLiberada = 0;
+
+const fases = document.querySelectorAll(".fase");
+
+const paginasDasFases = [
+  "indexPerguntasBiologia.html",
+  "#",
+  "#",
+  "#",
+];
+
+function atualizarFases() {
+  fases.forEach((fase, index) => {
+    fase.classList.remove("desbloqueada", "bloqueada");
+
+    if (index <= faseLiberada) {
+      fase.classList.add("desbloqueada");
+    } else {
+      fase.classList.add("bloqueada");
+    }
+  });
+}
+
+function abrirFase(index) {
+  if (index > faseLiberada) {
+    alert("Essa fase ainda está em desenvolvimento!");
+    return;
+  }
+
+  window.location.href = paginasDasFases[index];
+}
+
+atualizarFases();
